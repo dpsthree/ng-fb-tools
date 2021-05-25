264fade3e13afa0b31b6bc2e9f5ce9602e1595e5
Paul Spears <dpsthree@gmail.com>
2021-05-24 21:58:05 -0500

    initial commit

---
+++ apps/tooling-demo/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ng-fb-tools-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'tooling-demo';
+}
