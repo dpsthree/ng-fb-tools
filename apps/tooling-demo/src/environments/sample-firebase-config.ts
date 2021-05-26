e3a395dd4d4b1b5974cb85acc44e7af53a2de119
Paul Spears <dpsthree@gmail.com>
2021-05-25 19:58:03 -0500

    add firebase config

---
+++ apps/tooling-demo/src/environments/sample-firebase-config.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: '',
+  authDomain: 'ng-fb-tools.firebaseapp.com',
+  projectId: 'ng-fb-tools',
+  storageBucket: 'ng-fb-tools.appspot.com',
+  messagingSenderId: '267451027256',
+  appId: '1:267451027256:web:f5947447a265992cacbbcd',
+  measurementId: 'G-WM4RCWYGC1',
+};
