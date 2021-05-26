67809dd842a820a35c5998019f028e172c604784
Paul Spears <dpsthree@gmail.com>
2021-05-25 19:15:58 -0500

    put initial files in place for firebase

--- apps/tooling-demo/src/app/app.module.ts
+++ apps/tooling-demo/src/app/app.module.ts
@@ -1,12 +1,38 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { USE_EMULATOR as USE_AUTH_EMULATOR } from '@angular/fire/auth';
+import { USE_EMULATOR as USE_FIRESTORE_EMULATOR } from '@angular/fire/firestore';
+import {
+  ORIGIN as FUNCTIONS_ORIGIN,
+  NEW_ORIGIN_BEHAVIOR,
+  USE_EMULATOR as USE_FUNCTIONS_EMULATOR,
+} from '@angular/fire/functions';
 
+import { environment } from '../environments/environment';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule],
-  providers: [],
+  providers: [
+    {
+      provide: USE_AUTH_EMULATOR,
+      useValue: environment.production ? ['localhost', 9099] : undefined,
+    },
+    {
+      provide: USE_FIRESTORE_EMULATOR,
+      useValue: environment.production ? ['localhost', 8080] : undefined,
+    },
+    {
+      provide: USE_FUNCTIONS_EMULATOR,
+      useValue: environment.production ? ['localhost', 5001] : undefined,
+    },
+    { provide: NEW_ORIGIN_BEHAVIOR, useValue: true },
+    {
+      provide: FUNCTIONS_ORIGIN,
+      useFactory: () => (environment.production ? location.origin : undefined),
+    },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
